596fc83fb548910c33ad67e2b7b6d61161dacc8f
Sean Brage <seanmbrage@me.com>
2016-02-04 08:06:51 -0700

    Fetching meetups events in middleware before we render the dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
-
+var unirest = require('unirest');
 function Users(){
   return knex('users');
 }
@@ -9,15 +9,24 @@ function Users(){
 function Posts(){
   return knex('posts');
 }
+var events
+
+router.use('/', function(req, res, next) {
+  unirest.get('https://api.meetup.com/2/open_events?zip=80202&and_text=False&offset=0&format=json&limited_events=False&topic=javascript,angularjs,node,web+development,jquery,react,ember&photo-host=public&page=20&time=1w%2C2w&radius=25.0&desc=False&status=upcoming&sig_id=195744452&sig=77b786475090cc7600cdcad96aaea4c388daf3a9').end(function(response) {
+    events = response.body.results;
+    console.log(response.body.results);
+  })
+  next();
+});
 
 router.get('/', function (req, res, next) {
   res.cookie("logged out", "logged out")
   Users().fullOuterJoin("posts", "users.id", "posts.author_id").then(function (users) {
     if(req.session){
       var profile = res.locals.user
-      res.render('index', {title: "You are on Dashboard", users:users, profile: profile});
+      res.render('index', {title: "You are on Dashboard", users:users, profile: profile,events:events});
     } else {
-      res.render('index', {title: "You are on Dashboard", users:users, profile: profile});
+      res.render('index', {title: "You are on Dashboard", users:users, profile: profile,events:events});
     }
   })
 })
