9f0cdc28873f2d55f16f943849e586da24904942
Rachel Parri <erachelparri@gmail.com>
2016-02-03 17:14:00 -0700

    heroku

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.use(passport.session());
 passport.use(new LinkedInStrategy({
     consumerKey: process.env.LINKEDIN_CLIENT_ID,
     consumerSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    // callbackURL: "http://localhost:3000/auth/linkedin/callback" || "https://blooming-wave-60960.herokuapp.com/auth/linkedin/callback"
-    callbackURL: "https://blooming-wave-60960.herokuapp.com/auth/linkedin/callback" || "http://localhost:3000/auth/linkedin/callback"
+    callbackURL: "http://localhost:3000/auth/linkedin/callback" || "https://blooming-wave-60960.herokuapp.com/auth/linkedin/callback"
+    // callbackURL: "https://blooming-wave-60960.herokuapp.com/auth/linkedin/callback" || "http://localhost:3000/auth/linkedin/callback"
   },
   function(token, tokenSecret, profile, done) {
     Users().where('linkedin_id', profile.id).first().then(function (user) {
