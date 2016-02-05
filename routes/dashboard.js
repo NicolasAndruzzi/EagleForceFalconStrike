25b5cabe80b0e9bfeeecc81b025d2a1abb74bf16
Thomas Pasque <tpasque@gmail.com>
2016-02-05 11:39:52 -0700

    about updates, admin

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -103,18 +103,38 @@ router.get('/post/:post_id/upvote/show', function (req, res, next) {
 })
 router.get('/post/:post_id/downvote/show', function (req, res, next) {
   Comments().where('id', req.params.post_id).first().then(function (results) {
-    console.log(results);
     var votes = results.downvotes;
-    console.log(votes);
-    console.log("***********");
     votes += 1;
-    console.log(votes);
     Comments().where('id', req.params.post_id).update({
       downvotes: votes
     }).then(function (votes) {
       res.redirect('/posts/'+req.params.post_id+'/show')
     })
     })
+})
+
+// admin route for Super Admin
+router.get('/admin', function (req, res, next) {
+  var linkedinID = req.session.passport.user.id
+  Users().select().then(function (users) {
+    Users().where("linkedin_id", linkedinID).first().then(function (admin) {
+      console.log("^^^^TRUE^^^^");
+      console.log(admin);
+      Posts().select().then(function (posts) {
+        Posts().where("cat_name", "helps").then(function (helps) {
+          Posts().where("cat_name", "interestings").then(function (interestings) {
+            var interestingsNumber = Number(interestings.length);
+            var helpsNumber = Number(helps.length);
+            var userNumber = Number(users.length);
+            var profile = res.locals.user;
+            res.render('admin', {profile: profile, users: users, posts: posts, userNumber: userNumber, helpsNumber: helpsNumber,
+              interestingsNumber: interestingsNumber, admin: admin});
+
+            })
+        })
+      })
+    })
+  })
 })
 
 module.exports = router;
